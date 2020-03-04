47b658eac433fc6261697c3dfd8aae6a68670b59
macbookpro <abenov2005.uzer@gmail.com>
2020-03-04 18:07:49 +0600

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class BankAccount {
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        if (sum > 0) {
+            amount = amount + sum;
+            System.out.println(amount);
+        }
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (getAmount() < sum) {
+            throw new LimitException("Недостаточно средств на балансе. Остаток средств на балансе ", getAmount());
+        } else {
+            System.out.println(amount = amount - sum);
+            if (getAmount() < sum) {
+                System.out.println(amount = amount - amount);
+            }
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws LimitException {
+
+        BankAccount person1 = new BankAccount();
+        person1.deposit(10000);
+        for (int i = 0; ; i++) {
+            try {
+                person1.withDraw(6000);
+            } catch (LimitException LE) {
+                System.out.println(LE.getMessage());
+                break;
+            }
+        }
+    }
+}
